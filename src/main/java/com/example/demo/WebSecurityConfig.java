68692df90d6a5fde6c3350ee740c40962d007a77
Joaquin Gayoso-Cabada <jgayoso@ucm.es>
2020-04-10 13:44:27 +0200

    go go

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -37,5 +47,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			     .build();
 
 		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) 
+			throws Exception {
+			auth.jdbcAuthentication()
+			.dataSource(dataSource)
+//			.withDefaultSchema()
+//		      .withUser(User.withUsername("user")
+//		        .password(passwordEncoder().encode("pass"))
+//		        .roles("USER"))
+//		      .withUser(User.withUsername("gayoxo")
+//				        .password(passwordEncoder().encode("platano"))
+//				        .roles("USER"))
+			;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 }
